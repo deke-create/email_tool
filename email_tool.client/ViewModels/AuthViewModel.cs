526630284629561719f1e6d45f0740848f544fc8
deke-create <deke@mytechies.com>
2025-04-24 11:25:05 -0500

    Add ICommand implementations for SendEmail and Login actions
    Implemented `SendEmailCommand` in `EmailViewModel` and `LoginCommand` in `AuthViewModel` to handle asynchronous actions with parameters. Updated configuration with a more secure JWT key and refined code inspection exclusions in settings.

--- email_tool.client/ViewModels/AuthViewModel.cs
+++ email_tool.client/ViewModels/AuthViewModel.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using System.Windows.Input;
+using email_tool.client.Commands;
 using email_tool.client.Services;
 using email_tool.shared.Models;
 
@@ -8,9 +10,18 @@ public class AuthViewModel
 {
     private readonly AuthService _authService;
 
+    public ICommand LoginCommand { get; }
+
     public AuthViewModel(AuthService authService)
     {
         _authService = authService;
+        LoginCommand = new AsyncRelayCommand<object>(async (parameter) =>
+        {
+            if (parameter is (string username, string password))
+            {
+                await LoginAsync(username, password);
+            }
+        });
     }
 
     public async Task<CallResult<string>> LoginAsync(string username, string password)
@@ -21,6 +32,7 @@ public class AuthViewModel
             Password = password
         };
 
-        return await _authService.LoginAsync(loginRequest);
+        var res = await _authService.LoginAsync(loginRequest);
+        return res;
     }
 }
--- email_tool.client/ViewModels/EmailViewModel.cs
+++ email_tool.client/ViewModels/EmailViewModel.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using System.Windows.Input;
+using email_tool.client.Commands;
 using email_tool.client.Services;
 using email_tool.shared.Models;
 
@@ -8,9 +10,18 @@ public class EmailViewModel
 {
     private readonly EmailService _emailService;
 
+    public ICommand SendEmailCommand { get; }
+
     public EmailViewModel(EmailService emailService)
     {
         _emailService = emailService;
+        SendEmailCommand = new AsyncRelayCommand<object>(async (parameter) =>
+        {
+            if (parameter is (string sender, string recipient, string subject, string body))
+            {
+                await SendEmailAsync(sender, recipient, subject, body);
+            }
+        });
     }
 
     public async Task<CallResult<string>> SendEmailAsync(string sender, string recipient, string subject, string body)
